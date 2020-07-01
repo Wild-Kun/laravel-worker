19399a5250095c53ec8fbf427764ccfb40aecc1a
王坤 <1941483751@qq.com>
2020-07-01 18:28:58 +0800

    commit

---
+++ app/User.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * 用户头像
+     * @return mixed|string
+     */
+    public function avatar()
+    {
+        return "http://www.wangkun.live:8802/avatar/" . md5(strtolower($this->email)) . "?d=retro&s=64";
+    }
+}
